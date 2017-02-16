4da4ba8f9e1806007c18c4a7ae96a53d6d374336
Abel M. Osorio <abel.m.osorio@gmail.com>
2017-02-16 13:35:37 -0300

    Added some styles.

--- freelance-camp-fe/src/app/documents/documents.component.ts
+++ freelance-camp-fe/src/app/documents/documents.component.ts
@@ -5,6 +5,7 @@ import { Document } from './document';
 @Component({
   selector: 'documents',
   templateUrl: 'documents.component.html'
+  styleUrls: ['documents.component.css']
 })
 export class DocumentsComponent {
   pageTitle: string = 'Document Dashboard';
@@ -16,21 +17,21 @@ export class DocumentsComponent {
       description: 'Lorem ipsum dolor sit amet...',
       file_url:    'http://google.com',
       updated_at:  '14/02/17',
-      image_url:   'http://google.com'
+      image_url:   'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
     }, 
     {
       title:       'My second Doc',
       description: 'Lorem ipsum dolor sit amet...',
       file_url:    'http://google.com',
       updated_at:  '14/02/17',
-      image_url:   'http://google.com'
+      image_url:   'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
     }, 
     {
       title:       'My last Doc',
       description: 'Lorem ipsum dolor sit amet...',
       file_url:    'http://google.com',
       updated_at:  '14/02/17',
-      image_url:   'http://google.com'
+      image_url:   'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
     }
   ]
 
